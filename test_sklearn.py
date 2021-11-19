ce20eb440674fb36d238cd20cab999b1f6af2423
omervered0708 <omer.vered@campus.technion.ac.il>
2021-11-19 13:46:35 +0200

    + some functions

---
+++ test_sklearn.py
@@ -0,0 +1,40 @@
+
+import argparse
+import pandas as pd
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+
+def main():
+    print("*" * 20)
+    print("Started HW1_ID1_ID2_old.py")
+    # Parsing script arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('csv', type=str, help='Input csv file path')
+    parser.add_argument('k', type=int, help='k parameter')
+    parser.add_argument('p', type=float, help='p parameter')
+    args = parser.parse_args()
+
+    print("Processed input arguments:")
+    print(f"csv = {args.csv}, k = {args.k}, p = {args.p}")
+
+    print("Initiating KnnClassifier")
+    model = KNeighborsClassifier(k=args.k, p=args.p)
+    print(f"Loading data from {args.csv}...")
+    data = pd.read_csv(args.csv, header=None)
+    print(f"Loaded {data.shape[0]} rows and {data.shape[1]} columns")
+    X = data[data.columns[:-1]].values.astype(np.float32)
+    y = pd.factorize(data[data.columns[-1]])[0].astype(np.uint8)
+
+    print("Fitting...")
+    model.fit(X, y)
+    print("Done")
+    print("Predicting...")
+    y_pred = model.predict(X)
+    print("Done")
+    accuracy = np.sum(y_pred == y) / len(y)
+    print(f"Train accuracy: {accuracy * 100 :.2f}%")
+    print("*" * 20)
+
+
+if __name__ == "__main__":
+    main()
